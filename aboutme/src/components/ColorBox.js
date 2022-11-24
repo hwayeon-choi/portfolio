2a10d048e016a7f5021e661a925eade16a38f8a4
Hwayeon Choi <hwayeon.choi11@gmail.com>
2022-11-24 18:52:46 +0900

    Context API

---
+++ aboutme/src/components/ColorBox.js
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import ColorContext from '../contexts/color';
+
+const ColorBox = () => {
+  const { state } = useContext(ColorContext);
+  return (
+    <ColorConsumer>
+      <>
+        <div
+          style={{
+            width: '64px',
+            height: '64px',
+            background: state.color
+          }}
+        />
+        <div  
+          style={{
+            width: '32px',
+            height: '32px',
+            background: state.subcolor
+          }}
+        />
+      </>
+    </ColorConsumer>
+  );
+};
+
+export default ColorBox;
